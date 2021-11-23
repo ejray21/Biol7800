e75f5e22d4b08dc7c782a166e153251d567dd182
ejray21 <eray8@lsu.edu>
2021-11-23 12:24:25 -0600

    behavior analysis done

--- Project/Final Project Data Analysis.R
+++ Project/Final Project Data Analysis.R
@@ -13,3 +13,31 @@ Body.Mass.Aov <- aov(data$`mass (g)`~data$Treatment)
 TukeyHSD(Body.Mass.Aov)
 
 #Condition Factor
+Condition.Factor <- 100*(data$`mass (g)`/ (data$`standard length (mm)`^3))
+Condition.Factor.Aov <- aov(Condition.Factor~data$Treatment)
+TukeyHSD(Condition.Factor.Aov)
+
+##Comparing behaviors across treatment groups
+
+#distance traveled
+Distance.Traveled.Aov <- aov(data$`distance 30 min (mm)`~data$Treatment)
+TukeyHSD(Distance.Traveled.Aov)
+
+#speed
+speed.Aov <- aov(data$`toxtrac velocity 30 min (mm/s)`~data$Treatment)
+TukeyHSD(speed.Aov)
+
+#time in association zone
+AZ.time.Aov <- aov(data$`acclimation AZ time (s)`~data$Treatment)
+TukeyHSD(AZ.time.Aov)
+
+## I use an association index because multiple behaviors could be indicative of the fish's response
+
+#here I am multiplying the time spent in the association zone by the speed
+
+AI <- (data$`visible time in AZ (s)`* data$`toxtrac velocity 30 min (mm/s)`)
+AI.Aov <- aov(AI~data$Treatment)
+TukeyHSD(AI.Aov)
+
+
+
