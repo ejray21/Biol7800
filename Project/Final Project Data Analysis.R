e0ce0b1a6a48d871bcfe1d7fb3dc59b065ca280e
ejray21 <eray8@lsu.edu>
2021-11-23 14:11:36 -0600

    started making plots

--- Project/Final Project Data Analysis.R
+++ Project/Final Project Data Analysis.R
@@ -6,29 +6,31 @@ data <- POR_Master_Sheet
 
 #Standard Length
 Standard.Length.Aov <- aov(data$`standard length (mm)`~ data$Treatment)
+summary(Standard.Length.Aov)
 TukeyHSD(Standard.Length.Aov)
 
 #Body Mass
 Body.Mass.Aov <- aov(data$`mass (g)`~data$Treatment)
+summary(Body.Mass.Aov)
 TukeyHSD(Body.Mass.Aov)
 
 #Condition Factor
 Condition.Factor <- 100*(data$`mass (g)`/ (data$`standard length (mm)`^3))
 Condition.Factor.Aov <- aov(Condition.Factor~data$Treatment)
+summary(Condition.Factor.Aov)
 TukeyHSD(Condition.Factor.Aov)
 
 ##Comparing behaviors across treatment groups
 
 #distance traveled
 Distance.Traveled.Aov <- aov(data$`distance 30 min (mm)`~data$Treatment)
+summary(Distance.Traveled.Aov)
 TukeyHSD(Distance.Traveled.Aov)
 
-#speed
-speed.Aov <- aov(data$`toxtrac velocity 30 min (mm/s)`~data$Treatment)
-TukeyHSD(speed.Aov)
 
 #time in association zone
 AZ.time.Aov <- aov(data$`acclimation AZ time (s)`~data$Treatment)
+Summary(AZ.time.Aov)
 TukeyHSD(AZ.time.Aov)
 
 ## I use an association index because multiple behaviors could be indicative of the fish's response
@@ -37,7 +39,20 @@ TukeyHSD(AZ.time.Aov)
 
 AI <- (data$`visible time in AZ (s)`* data$`toxtrac velocity 30 min (mm/s)`)
 AI.Aov <- aov(AI~data$Treatment)
+Summary(AI.Aov)
 TukeyHSD(AI.Aov)
 
 
+### Making graphs ###
+treatment <- factor(data$Treatment, levels=c("Control", "Chemosensory", "Visual", "C + V"))
+library(ggplot2)
+
+## Condition factor plot ##
+condition.factor.plot <- ggplot(data, aes(x=treatment, y = Condition.Factor, fill= treatment))+ geom_boxplot() + labs(y = "Condition Factor", x = " ") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+condition.factor.plot
+
+## Distance Traveled Plot ##
+distance <- data$`distance 30 min (mm)`
+distance.plot <- ggplot(data, aes(x=treatment, y = distance, fill= treatment)) + geom_boxplot() + labs(y = "Total Distance Traveled (mm)", x = " ") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+distance.plot
 
