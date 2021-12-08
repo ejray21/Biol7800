f752720cc0100f63161069003f5a794cdc4cd4b8
ejray21 <eray8@lsu.edu>
2021-12-07 19:06:14 -0600

    Added figures, updated results. Almost done!

--- Project/Final Project Data Analysis.R
+++ Project/Final Project Data Analysis.R
@@ -33,10 +33,7 @@ Standard.Length <- (data.table$`standard length (mm)`)
 Condition.Factor <- na.omit(Condition.Factor)
 ##create table
 body.stats.table <- tabular((treatment.table + 1) ~ (n=1) + Format(digits = 2)* (Body.Mass + Standard.Length + Condition.Factor)*(mean + sd), data.table)
-## Using booktabs to make the table look nice
-final.dissection.table <- booktabs(body.stats.table)
-final.dissection.table
-latex.dissection.table <- toLatex(body.stats.table)
+body.stats.table
 
 ##Comparing behaviors across treatment groups
 
@@ -44,11 +41,11 @@ latex.dissection.table <- toLatex(body.stats.table)
 Distance.Traveled.Aov <- aov(data$`distance 30 min (mm)`~data$Treatment)
 summary(Distance.Traveled.Aov)
 TukeyHSD(Distance.Traveled.Aov)
-distance.f <- Distance.Traveled.Aov$
+
 
 #time in association zone
 AZ.time.Aov <- aov(data$`acclimation AZ time (s)`~data$Treatment)
-Summary(AZ.time.Aov)
+summary(AZ.time.Aov)
 TukeyHSD(AZ.time.Aov)
 
 ## I use an association index because multiple behaviors could be indicative of the fish's response
@@ -56,24 +53,18 @@ TukeyHSD(AZ.time.Aov)
 
 ## get association index for first 10 mins of trial
 AI <- (data$`hand calculated time in AZ first ten mins (s)`* data$`association score first 10 mins`)
-AI.Aov <- aov(AI~data$Treatment)
-Summary(AI.Aov)
-TukeyHSD(AI.Aov)
+Ai.Aov <- aov(AI~data$Treatment)
+summary(Ai.Aov)
+TukeyHSD(Ai.Aov)
+
 
 ## now get association index for the pretrial 10 min (before stimulus delivery)
 Acc.AI <- (data$`acclimation AZ time (s)`*data$`acclimation association score`)
 Acc.AI.Aov <- aov(Acc.AI~data$Treatment)
-Summary(Acc.AI.Aov)
+summary(Acc.AI.Aov)
 TukeyHSD(Acc.AI.Aov)
 ## There shouldn't be any differences here since no stimulus was delivered yet
 
-## Now calculating the percent change in the AI from before stimulus delivery to after
-Percent.Change.Ai <- ((AI-Acc.AI)/Acc.AI)
-Percent.Change.AI <- na.omit(Percent.Change.Ai)
-library(dplyr)
-data.Ai.change <- data %>% filter(!is.na(Percent.Change.Ai))
-df <- data.Ai.change[!is.infinite(rowSums(Percent.Change.AI)),]
-Per.Change.AI.Aov <- aov(Percent.Change.AI~data.Ai.change$Treatment)
 
 ### Making graphs ###
 treatment <- factor(data$Treatment, levels=c("Control", "Chemosensory", "Visual", "C + V"))
@@ -95,25 +86,45 @@ AZ.time.plot <- ggplot(data, aes(x=treatment, y = AZ.time, fill= treatment)) + g
 AZ.time.plot
 
 ##Association Index Plot ##
-AI.plot <- ggplot(data, aes(x=treatment, y = AI, fill= treatment)) + geom_boxplot() + labs(y = "Association Index", x = " ") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+AI.plot <- ggplot(data, aes(x=treatment, y = AI, fill= treatment)) + geom_boxplot() + labs(y = "Association Index", x = " ") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))+geom_jitter(color="black", size=0.4, alpha=0.9)
 AI.plot
 
-##DC 5 Plot ##
+### Brain activation analysis
+
+
+##DC 5 Anova ##
+##First filter data so only rows that have values for the dc4 and 5 are present
 data.dc <- data %>% filter(!is.na(data$`dc-5 cell count`))
 treatment <- factor(data.dc$Treatment, levels=c("Control", "Chemosensory", "Visual", "C + V"))
+dc5.aov <- aov(data.dc$`dc-5 cell count`~treatment)
+summary(dc5.aov)
+TukeyHSD(dc5.aov)
+
+## DC 5 plot ##
 dc5.plot <- ggplot(data.dc, aes(x=treatment, y = data.dc$`dc-5 cell count`, fill= treatment)) + geom_boxplot() + labs(y= expression(paste('pS6 Stained Cells/', mu,~m^2)), x = ' ')+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))+
   geom_jitter(color="black", size=0.4, alpha=0.9)+ ylim(0,0.0008)
 dc5.plot
+
+#dc 4 anova##
+dc4.aov <- aov(data.dc$`dc-4 cell count`~treatment)
+summary(dc4.aov)
+TukeyHSD(dc4.aov)
 
 ##DC 4 Plot##
 dc4.plot <- ggplot(data.dc, aes(x=treatment, y = data.dc$`dc-4 cell count`, fill= treatment)) + geom_boxplot() + labs(y= expression(paste('pS6 Stained Cells/', mu,~m^2)), x = ' ')+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))+
   geom_jitter(color="black", size=0.4, alpha=0.9)+ ylim(0,0.0009) 
 dc4.plot
 
-##TPp Plot
+##TPp anova
+# first filter data so only rows that have data for the tpp are present
 data.tpp <- data %>% filter(!is.na(data$`tpp cell count`))
 tpp <- data.tpp$`tpp cell count`
 treatment <- factor(data.tpp$Treatment, levels=c("Control", "Chemosensory", "Visual", "C + V"))
+tpp.aov <- aov(tpp~treatment)
+summary (tpp.aov)
+TukeyHSD(tpp.aov)
+
+#tpp plot
 tpp.plot <- ggplot(data.tpp, aes(x=treatment, y = tpp, fill= treatment)) + geom_boxplot() + labs(y= expression(paste('pS6 Stained Cells/', mu,~m^2)), x = ' ')+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=0.5))+
   geom_jitter(color="black", size=0.4, alpha=0.9)+ ylim(0, 0.00125)
 tpp.plot
